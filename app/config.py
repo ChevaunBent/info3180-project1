9ad2078dd5dbfac6c260f01053a8bab755fc9c7c
Vaun <Vaun@Chevaun.local>
2021-03-23 13:13:01 -0500

    Removed buggs relating to heroku connection

--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Scr3tk3y'
+    # By default Heroku creates its own DATABASE_URL environment variable so we cannot select this name
+    # We will then need to set our environment variable locally to DATABASE_URL_ and also on Heroku
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://username@localhost/database' or os.environ.get('DATABASE_URL_')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
     UPLOAD_FOLDER = 'app/static/uploads'
